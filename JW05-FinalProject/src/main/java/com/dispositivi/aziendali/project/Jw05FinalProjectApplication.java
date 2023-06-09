700c53aa1c9c86c7216b8dacd2bf86c42a883fc8
promi93 <116350522+promi93@users.noreply.github.com>
2023-06-09 18:14:28 +0200

    w05

---
+++ JW05-FinalProject/src/main/java/com/dispositivi/aziendali/project/Jw05FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dispositivi.aziendali.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jw05FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jw05FinalProjectApplication.class, args);
+	}
+
+}
---
+++ JW05-FinalProject/src/test/java/com/dispositivi/aziendali/project/Jw05FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dispositivi.aziendali.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jw05FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
